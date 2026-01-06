c3d29b8e4137bc2b1154d1c37862f1efa871bf0a
clarateixeirahancock <clara.teixeirahancock@gmail.com>
2026-01-06 20:41:38 +0000

    Refactor SHIPPING_RATES to use objects for regions

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -7,16 +7,18 @@ const PRODUCTS = {
 };
 
 // Shipping rates
-const SHIPPING_RATES = [
-  // UK
-  { shipping_rate: "shr_GB_10" },
-  { shipping_rate: "shr_GB_20" },
-  { shipping_rate: "shr_GB_40" },
-  // Worldwide
-  { shipping_rate: "shr_WW_10" },
-  { shipping_rate: "shr_WW_20" },
-  { shipping_rate: "shr_WW_40" }
-];
+const SHIPPING_RATES = {
+  GB: [
+    { maxWeight: 0.05, rate: "shr_1SmepTLp5l1JmABsJzFF773I" },
+    { maxWeight: 0.10, rate: "shr_1Smes2Lp5l1JmABs2eSRdmI9" },
+    { maxWeight: 0.20, rate: "shr_1SmgR0Lp5l1JmABsJVkE4raC" }
+  ],
+  WW: [
+    { maxWeight: 0.05, rate: "shr_1Smeq6Lp5l1JmABsxxy2qNRv" },
+    { maxWeight: 0.10, rate: "shr_1SmgQgLp5l1JmABssFDuJ3Nn" },
+    { maxWeight: 0.20, rate: "shr_1SmgRJLp5l1JmABsc7qmBqit" }
+  ]
+};
 
 exports.handler = async (event) => {
   const headers = {
