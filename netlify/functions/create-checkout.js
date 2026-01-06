f3c259fd574cd4ef6583f629799a0fd3835b4f41
clarateixeirahancock <clara.teixeirahancock@gmail.com>
2026-01-06 21:07:58 +0000

    Update create-checkout.js

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -7,18 +7,14 @@ const PRODUCTS = {
 };
 
 // Shipping rates
-const SHIPPING_RATES = {
-  GB: [
-    { maxWeight: 0.05, rate: "shr_1SmepTLp5l1JmABsJzFF773I" },
-    { maxWeight: 0.10, rate: "shr_1Smes2Lp5l1JmABs2eSRdmI9" },
-    { maxWeight: 0.20, rate: "shr_1SmgR0Lp5l1JmABsJVkE4raC" }
-  ],
-  WW: [
-    { maxWeight: 0.05, rate: "shr_1Smeq6Lp5l1JmABsxxy2qNRv" },
-    { maxWeight: 0.10, rate: "shr_1SmgQgLp5l1JmABssFDuJ3Nn" },
-    { maxWeight: 0.20, rate: "shr_1SmgRJLp5l1JmABsc7qmBqit" }
-  ]
-};
+const shipping_options = [
+  { shipping_rate: "shr_1SmepTLp5l1JmABsJzFF773I" }, // 10 postcards UK
+  { shipping_rate: "shr_1Smes2Lp5l1JmABs2eSRdmI9" }, // 20 postcards UK
+  { shipping_rate: "shr_1SmgR0Lp5l1JmABsJVkE4raC" }, // 40 postcards UK
+  { shipping_rate: "shr_1Smeq6Lp5l1JmABsxxy2qNRv" }, // 10 postcards WW
+  { shipping_rate: "shr_1SmgQgLp5l1JmABssFDuJ3Nn" }, // 20 postcards WW
+  { shipping_rate: "shr_1SmgRJLp5l1JmABsc7qmBqit" }  // 40 postcards WW
+];
 
 exports.handler = async (event) => {
   const headers = {
